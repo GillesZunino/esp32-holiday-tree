bc87f50c4ae56f20ca50b5c9046914052573e89b
Gilles Zunino <gilleszunino@users.noreply.github.com>
2024-05-04 21:19:05 -0700

    Capture configuration options in a static string which can be logged during app_main()

---
+++ main/compile_options.h
@@ -0,0 +1,29 @@
+// -----------------------------------------------------------------------------------
+// Copyright 2024, Gilles Zunino
+// -----------------------------------------------------------------------------------
+
+#pragma once
+
+// Static string listing all compile options
+const char* CompileTimeOptions =
+    "[" __TIMESTAMP__ "] "
+    #if CONFIG_HOLIDAYTREE_HARDWARE_PRODUCTION
+        "HARDWARE_PRODUCTION"
+    #else
+        "HARDWARE_DEVELOPMENT"
+    #endif
+
+    "|BR_EDR_DEVICE_NAME_STR '" CONFIG_HOLIDAYTREE_BR_EDR_DEVICE_NAME_STR "'"
+
+    #if CONFIG_HOLIDAYTREE_BR_EDR_LEGACY_PAIRING_REQUIRE_STATIC_PIN
+        #if CONFIG_HOLIDAYTREE_HARDWARE_PRODUCTION
+            "|PAIRING_REQUIRE_STATIC_PIN <HIDDEN>"
+        #else
+            #if CONFIG_HOLIDAYTREE_HARDWARE_DEVELOPMENT
+                "|PAIRING_REQUIRE_STATIC_PIN " CONFIG_HOLIDAYTREE_BR_EDR_STATIC_PIN_STR
+            #endif
+        #endif
+    #else
+        "|PAIRING_REQUIRE_STATIC_PIN OFF"
+    #endif
+;
