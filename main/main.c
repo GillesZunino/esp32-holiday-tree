96e0bda9453216d17d1e19e96714b81eaae48d08
Gilles Zunino <gilleszunino@users.noreply.github.com>
2024-07-06 21:17:07 -0700

    Main program entry point

---
+++ main/main.c
@@ -0,0 +1,94 @@
+// -----------------------------------------------------------------------------------
+// Copyright 2024, Gilles Zunino
+// -----------------------------------------------------------------------------------
+
+#include "sdkconfig.h"
+
+#include <freertos/FreeRTOS.h>
+#include <freertos/task.h>
+
+
+#include <esp_log.h>
+#include <nvs.h>
+#include <nvs_flash.h>
+
+
+#include "compile_options.h"
+
+#include "gpio/gpio_init.h"
+
+#include "button/button_init.h"
+
+#include "leds/led_init.h"
+#include "leds/led_animator.h"
+
+#include "bt/bt_init.h"
+
+
+
+//
+// We assign different GPIO pins to various peripherials in development versus production:
+//  * [Real Device]
+//      * Momentary button on IO15 which is also JTAG TDO and a strapping pin
+//      * WS2812 LED data on IO5 which is also a strapping pin
+//  * [Development]
+//      * Momentary button on IO23 which has no conflict and allows JTAG use
+//      * WS2812 LED data on IO22 which has no conflict
+//
+#if CONFIG_HOLIDAYTREE_HARDWARE_PRODUCTION
+    // Momentary button GPIO
+    const gpio_num_t BUTTON_GPIO = GPIO_NUM_15;
+
+    // Individually adressable LEDs data GPIO
+    const gpio_num_t LED_DATA_GPIO = GPIO_NUM_5;
+#else
+    // Momentary button GPIO
+    const gpio_num_t BUTTON_GPIO = GPIO_NUM_23;
+
+    // Individually adressable LEDs data GPIO
+    const gpio_num_t LED_DATA_GPIO = GPIO_NUM_22;
+#endif
+
+
+// Individually adressable LEDs on/off switch GPIO
+const gpio_num_t LED_SWITCH_GPIO = GPIO_NUM_4;
+
+// Main application log tag
+static const char* MAIN_TAG = "app_main";
+
+
+
+static void on_button_pressed(void) {
+    // Currently no action
+}
+
+
+void app_main(void) {
+    // Log compile time options
+    ESP_LOGI(MAIN_TAG, "%s", CompileTimeOptions);
+
+    // Initialize NVS — It is used to store PHY calibration data
+    esp_err_t err = nvs_flash_init();
+    if ((err == ESP_ERR_NVS_NO_FREE_PAGES) || (err == ESP_ERR_NVS_NEW_VERSION_FOUND)) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        err = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK(err);
+
+    // Configure GPIO pin interrupts
+    ESP_ERROR_CHECK(configure_gpio_isr_dispatcher());
+
+    // Configure Bluetooth Classic and start A2DP profile for tree sound player
+    ESP_ERROR_CHECK(configure_bluetooth());
+
+    // Configure tree momentary button
+    ESP_ERROR_CHECK(configure_button(BUTTON_GPIO, &on_button_pressed));
+
+    // Configure tree lights
+    ESP_ERROR_CHECK(configure_led_strip(LED_DATA_GPIO, LED_SWITCH_GPIO));
+    ESP_ERROR_CHECK(start_led_strip_effect(ProgressiveReveal));
+
+    while (true) {
+        vTaskDelay(pdMS_TO_TICKS(10 * 1000));
+    }
+}
