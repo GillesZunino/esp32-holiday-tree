094806fb27323e5e9391305b52b65d1a68fa4d33
Gilles Zunino <gilleszunino@users.noreply.github.com>
2024-10-13 20:33:00 -0700

    Add configuration setting (HOLIDAYTREE_BT_GAP_LOG) - Enabled logs in GAP based on setting

--- main/bt/bt_gap.c
+++ main/bt/bt_gap.c
@@ -30,9 +30,11 @@ static void bt_gap_callback(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *
     switch (event) {
         case ESP_BT_GAP_AUTH_CMPL_EVT: {
             if (param->auth_cmpl.stat == ESP_BT_STATUS_SUCCESS) {
+#if CONFIG_HOLIDAYTREE_BT_GAP_LOG
                 char bdaStr[18];
                 ESP_LOGI(BtGapTag, "ESP_BT_GAP_AUTH_CMPL_EVT authenticated with [%s], name '%s'", get_bda_string(param->auth_cmpl.bda, bdaStr), param->auth_cmpl.device_name);
                 ESP_LOGI(BtGapTag, "ESP_BT_GAP_AUTH_CMPL_EVT link key type of current link is %s (%d)", get_gap_link_key_type_name(param->auth_cmpl.lk_type), param->auth_cmpl.lk_type);
+#endif
             } else {
                 ESP_LOGE(BtGapTag, "ESP_BT_GAP_AUTH_CMPL_EVT failed %d", param->auth_cmpl.stat);
             }
@@ -41,62 +43,78 @@ static void bt_gap_callback(esp_bt_gap_cb_event_t event, esp_bt_gap_cb_param_t *
 
         case ESP_BT_GAP_CONFIG_EIR_DATA_EVT:
             if (param->config_eir_data.stat == ESP_BT_STATUS_SUCCESS) {
+#if CONFIG_HOLIDAYTREE_BT_GAP_LOG
                 ESP_LOGI(BtGapTag, "ESP_BT_GAP_CONFIG_EIR_DATA_EVT success - EIR type count %d", param->config_eir_data.eir_type_num);
                 for (uint8_t typeIndex = 0; typeIndex < param->config_eir_data.eir_type_num; typeIndex++) {
                     ESP_LOGI(BtGapTag, "\tEIR_TYPE[%d]: %s (%d)", typeIndex, get_eir_name(param->config_eir_data.eir_type[typeIndex]), param->config_eir_data.eir_type[typeIndex]);
                 }
+#endif
             } else {
-                ESP_LOGI(BtGapTag, "ESP_BT_GAP_CONFIG_EIR_DATA_EVT failed %d", param->config_eir_data.stat);
+                ESP_LOGE(BtGapTag, "ESP_BT_GAP_CONFIG_EIR_DATA_EVT failed %d", param->config_eir_data.stat);
             }
         break;
 
         case ESP_BT_GAP_SET_AFH_CHANNELS_EVT:
+#if CONFIG_HOLIDAYTREE_BT_GAP_LOG
             ESP_LOGI(BtGapTag, "ESP_BT_GAP_SET_AFH_CHANNELS_EVT: %d", param->set_afh_channels.stat);
+#endif
         break;
 
         case ESP_BT_GAP_READ_REMOTE_NAME_EVT: {
             if (param->config_eir_data.stat == ESP_BT_STATUS_SUCCESS) {
+#if CONFIG_HOLIDAYTREE_BT_GAP_LOG
                 char bdaStr[18];
                 ESP_LOGI(BtGapTag, "ESP_BT_GAP_READ_REMOTE_NAME_EVT remote device [%s], name '%s'", get_bda_string(param->read_rmt_name.bda, bdaStr), param->read_rmt_name.rmt_name);
+#endif
             } else {
-                ESP_LOGI(BtGapTag, "ESP_BT_GAP_READ_REMOTE_NAME_EVT failed %d", param->read_rmt_name.stat);
+                ESP_LOGE(BtGapTag, "ESP_BT_GAP_READ_REMOTE_NAME_EVT failed %d", param->read_rmt_name.stat);
             }
         }
         break;
 
         case ESP_BT_GAP_MODE_CHG_EVT:
+#if CONFIG_HOLIDAYTREE_BT_GAP_LOG
             ESP_LOGI(BtGapTag, "ESP_BT_GAP_MODE_CHG_EVT mode %s (0x%x)", get_gap_power_mode_name(param->mode_chg.mode), param->mode_chg.mode);
+#endif
         break;
 
         case ESP_BT_GAP_REMOVE_BOND_DEV_COMPLETE_EVT:
             if (param->remove_bond_dev_cmpl.status == ESP_BT_STATUS_SUCCESS) {
+#if CONFIG_HOLIDAYTREE_BT_GAP_LOG
                 char bdaStr[18];
                 ESP_LOGI(BtGapTag, "ESP_BT_GAP_REMOVE_BOND_DEV_COMPLETE_EVT remote device [%s] success", get_bda_string(param->remove_bond_dev_cmpl.bda, bdaStr));
+#endif
             } else {
                 ESP_LOGE(BtGapTag, "ESP_BT_GAP_REMOVE_BOND_DEV_COMPLETE_EVT failed %d", param->remove_bond_dev_cmpl.status);
             }
         break;
 
         case ESP_BT_GAP_ACL_CONN_CMPL_STAT_EVT: {
+#if CONFIG_HOLIDAYTREE_BT_GAP_LOG
             char bdaStr[18];
             ESP_LOGI(BtGapTag, "ESP_BT_GAP_ACL_CONN_CMPL_STAT_EVT connected to [%s], status 0x%02x", get_bda_string(param->acl_conn_cmpl_stat.bda, bdaStr), param->acl_conn_cmpl_stat.stat);
+#endif
         }
         break;
         
         case ESP_BT_GAP_ACL_DISCONN_CMPL_STAT_EVT: {
+#if CONFIG_HOLIDAYTREE_BT_GAP_LOG
             char bdaStr[18];
             ESP_LOGI(BtGapTag, "ESP_BT_GAP_ACL_DISC_CMPL_STAT_EVT disconnected from [%s], reason 0x%02x", get_bda_string(param->acl_disconn_cmpl_stat.bda, bdaStr), param->acl_disconn_cmpl_stat.reason);
+#endif
         }
         break;
 
         case ESP_BT_GAP_ENC_CHG_EVT: {
+#if CONFIG_HOLIDAYTREE_BT_GAP_LOG
             char bdaStr[18];
             ESP_LOGI(BtGapTag, "ESP_BT_GAP_ENC_CHG_EVT encryption mode to [%s] changed to %s", get_bda_string(param->enc_chg.bda, bdaStr), get_gap_encryption_mode(param->enc_chg.enc_mode));
+#endif
         }
         break;
 
         default: {
-            ESP_LOGI(BtGapTag, "bt_gap_callback() received unknown event '%d'", event);
+            ESP_LOGW(BtGapTag, "bt_gap_callback() received unknown event '%d'", event);
             break;
         }
     }
