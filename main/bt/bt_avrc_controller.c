346580a955ae067b65f95c7bf7432ee2ca107012
Gilles Zunino <gilleszunino@users.noreply.github.com>
2024-10-13 20:51:15 -0700

    Add configuration setting (HOLIDAYTREE_BT_AVR_CT_LOG) - Enabled logs in AVR Controller based on setting

--- main/bt/bt_avrc_controller.c
+++ main/bt/bt_avrc_controller.c
@@ -22,7 +22,7 @@ static const char* BtAvrcControllerTag = "bt_avrc_ct";
 
 // Commands Transaction Labels = These must be the same across notifications - There is a maximum of 15 transaction labels
 static const uint8_t CommandGetCapabilitiesTransactionLabel = 0;
-static const uint8_t COmmandGetMetadataTransactionLabel = 1;
+static const uint8_t CommandGetMetadataTransactionLabel = 1;
 
 // Notification Transaction Labels = These must be the same across notifications - There is a maximum of 15 transaction labels
 static const uint8_t NotificationTrackChangeTransactionLabel = 2;
@@ -80,10 +80,11 @@ static void avrc_controller_event_handler(uint16_t event, void* param) {
     esp_avrc_ct_cb_param_t* callbackParams = (esp_avrc_ct_cb_param_t *) param;
     switch (event) {
         case ESP_AVRC_CT_CONNECTION_STATE_EVT: {
+#if CONFIG_HOLIDAYTREE_BT_AVR_CT_LOG
             char bdaStr[18];
             ESP_LOGI(BtAvrcControllerTag, "[CT] ESP_AVRC_CT_CONNECTION_STATE_EVT %s remote [%s]",
                     callbackParams->conn_stat.connected ? "connected to" : "disconnected from", get_bda_string(callbackParams->conn_stat.remote_bda, bdaStr));
-            
+#endif
             // When connected, retrieve remote AVRC TG supported notification events so we can subscribe to notifications (play, pause ...)
             s_avrc_peer_notifications_capabilities.bits = 0;
             if (callbackParams->conn_stat.connected) {
@@ -95,8 +96,10 @@ static void avrc_controller_event_handler(uint16_t event, void* param) {
         break;
 
         case ESP_AVRC_CT_METADATA_RSP_EVT: {
+#if CONFIG_HOLIDAYTREE_BT_AVR_CT_LOG            
             uint8_t attributeId = callbackParams->meta_rsp.attr_id;
             ESP_LOGI(BtAvrcControllerTag, "[CT] ESP_AVRC_CT_METADATA_RSP_EVT -> %s (0x%x): '%s'", get_avrc_metdata_attribute_name(attributeId), attributeId , callbackParams->meta_rsp.attr_text);
+#endif
             if (callbackParams->meta_rsp.attr_text != NULL) {
                 // Free memory we allocated for the metadata string
                 free(callbackParams->meta_rsp.attr_text);
@@ -106,12 +109,16 @@ static void avrc_controller_event_handler(uint16_t event, void* param) {
         break;
 
         case ESP_AVRC_CT_PLAY_STATUS_RSP_EVT: {
+#if CONFIG_HOLIDAYTREE_BT_AVR_CT_LOG
             ESP_LOGI(BtAvrcControllerTag, "[CT] ESP_AVRC_CT_PLAY_STATUS_RSP_EVT");
+#endif
         }
         break;
 
        case ESP_AVRC_CT_CHANGE_NOTIFY_EVT: {
+#if CONFIG_HOLIDAYTREE_BT_AVR_CT_LOG
             ESP_LOGI(BtAvrcControllerTag, "[CT] ESP_AVRC_CT_CHANGE_NOTIFY_EVT -> %s (0x%x)", get_avrc_notification_name(callbackParams->change_ntf.event_id), callbackParams->change_ntf.event_id);
+#endif
             if (handle_controller_notification_event(callbackParams->change_ntf.event_id, &callbackParams->change_ntf.event_parameter) != ESP_OK) {
                 ESP_LOGE(BtAvrcControllerTag, "[CT] ESP_AVRC_CT_CHANGE_NOTIFY_EVT failed in handle_controller_notification_event()");
             }
@@ -119,6 +126,7 @@ static void avrc_controller_event_handler(uint16_t event, void* param) {
         break;
 
         case ESP_AVRC_CT_REMOTE_FEATURES_EVT: {
+#if CONFIG_HOLIDAYTREE_BT_AVR_CT_LOG
             ESP_LOGI(BtAvrcControllerTag, "[CT] ESP_AVRC_CT_REMOTE_FEATURES_EVT feature bit mask: 0x%"PRIx32", TG features: 0x%"PRIx16, callbackParams->rmt_feats.feat_mask, callbackParams->rmt_feats.tg_feat_flag);
             
             // Features
@@ -136,12 +144,14 @@ static void avrc_controller_event_handler(uint16_t event, void* param) {
             for (uint8_t index = 0; (featureFlagsStr[index] != NULL) && (index < 8); index++) {
                 ESP_LOGI(BtAvrcControllerTag, "[CT]\t%s", featureFlagsStr[index]);
             }
+#endif
         }
         break;
 
         case ESP_AVRC_CT_GET_RN_CAPABILITIES_RSP_EVT: {
+#if CONFIG_HOLIDAYTREE_BT_AVR_CT_LOG
             ESP_LOGI(BtAvrcControllerTag, "[CT] ESP_AVRC_CT_GET_RN_CAPABILITIES_RSP_EVT remote rn_cap: count %d, bitmask 0x%x", callbackParams->get_rn_caps_rsp.cap_count, callbackParams->get_rn_caps_rsp.evt_set.bits);
-
+#endif
             // Capture remote controller capabilities - We will need them when a notification is received and we need to re-attach the event notification
             s_avrc_peer_notifications_capabilities.bits = callbackParams->get_rn_caps_rsp.evt_set.bits;
             
@@ -153,14 +163,16 @@ static void avrc_controller_event_handler(uint16_t event, void* param) {
         break;
 
         case ESP_AVRC_CT_SET_ABSOLUTE_VOLUME_RSP_EVT: {
+#if CONFIG_HOLIDAYTREE_BT_AVR_CT_LOG
             uint8_t volumeAvrc = callbackParams->set_volume_rsp.volume;
             uint16_t volumePercent = AVRC_VOLUME_TO_PERCENT(volumeAvrc);
             ESP_LOGI(BtAvrcControllerTag, "[CT] ESP_AVRC_CT_SET_ABSOLUTE_VOLUME_RSP_EVT volume: %d (%d%%)", volumeAvrc, volumePercent);
+#endif
         }
         break;
 
         default:
-            ESP_LOGI(BtAvrcControllerTag, "%s() [CT] received unknown event: %d", __func__, event);
+            ESP_LOGW(BtAvrcControllerTag, "%s() [CT] received unknown event: %d", __func__, event);
             break;
     }
 }
@@ -168,19 +180,25 @@ static void avrc_controller_event_handler(uint16_t event, void* param) {
 static esp_err_t handle_controller_notification_event(uint8_t event, esp_avrc_rn_param_t *params) {
     switch (event) {
         case ESP_AVRC_RN_PLAY_STATUS_CHANGE: {
+#if CONFIG_HOLIDAYTREE_BT_AVR_CT_LOG
             ESP_LOGI(BtAvrcControllerTag, "[CT] [NOTIFY] ESP_AVRC_RN_PLAY_STATUS_CHANGE: Playback status changed: 0x%x", params->playback);
+#endif
             return register_for_notification(ESP_AVRC_RN_PLAY_STATUS_CHANGE, NotificationPlaybackChangeTransactionLabel, 0);
         }
 
         case ESP_AVRC_RN_TRACK_CHANGE: {
+#if CONFIG_HOLIDAYTREE_BT_AVR_CT_LOG
             ESP_LOGI(BtAvrcControllerTag, "[CT] [NOTIFY] ESP_AVRC_RN_TRACK_CHANGE");
-            esp_err_t send_err = esp_avrc_ct_send_metadata_cmd(COmmandGetMetadataTransactionLabel, ESP_AVRC_MD_ATTR_TITLE | ESP_AVRC_MD_ATTR_ARTIST | ESP_AVRC_MD_ATTR_ALBUM | ESP_AVRC_MD_ATTR_GENRE);
+#endif
+            esp_err_t send_err = esp_avrc_ct_send_metadata_cmd(CommandGetMetadataTransactionLabel, ESP_AVRC_MD_ATTR_TITLE | ESP_AVRC_MD_ATTR_ARTIST | ESP_AVRC_MD_ATTR_ALBUM | ESP_AVRC_MD_ATTR_GENRE);
             esp_err_t register_err = register_for_notification(ESP_AVRC_RN_TRACK_CHANGE, NotificationTrackChangeTransactionLabel, 0);
             return send_err == ESP_OK ? register_err : send_err;
         }
 
         case ESP_AVRC_RN_PLAY_POS_CHANGED: {
+#if CONFIG_HOLIDAYTREE_BT_AVR_CT_LOG
             ESP_LOGI(BtAvrcControllerTag, "[CT] [NOTIFY] ESP_AVRC_RN_PLAY_POS_CHANGED: Play position changed: %"PRIu32"-ms", params->play_pos);
+#endif
             return register_for_notification(ESP_AVRC_RN_PLAY_POS_CHANGED, NotificationPlayPositionChangedTransactionLabel, 10);
         }
 
